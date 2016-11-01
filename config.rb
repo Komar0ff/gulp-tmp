e2b3055ebb67aa2c5bdeb43b65c25df35fa89f3e
Ilya_shurikov <binary.reptile@gmail.com>
2016-11-02 00:33:19 +0300

    add local server and compass

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "build/css"
+sass_dir = "sass"
+images_dir = "img"
+output_style = :compact
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    minify = require('gulp-clean-css'),
+    compass = require('gulp-compass'),
+    sync = require('browser-sync')
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    tinypng = require('gulp-tinypng'),
+    uglify = require('gulp-uglify'),
+    minifycss = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer');
 
-gulp.task("sass", function() {
-    gulp.src(['src/sass/*.scss', 'src/sass/*.sass'])
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(minify())
-    .pipe(gulp.dest('build/css'));
-});
+      gulp.task('connect', function() {
+          connect.server({root: 'src', livereload: true});
+      });
+
+      gulp.task('styles', function() {
+          gulp.src(['src/sass/*.scss']).pipe(compass({config_file: 'config.rb', sass: 'src/sass'})).pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')).pipe(rename({suffix: '.min'})).pipe(minifycss()).pipe(gulp.dest('build/css')).pipe(connect.reload());
+      });
+
+      gulp.task('scripts', function() {
+          return gulp.src('src/js/*.js').pipe(concat('main.js')).pipe(rename({suffix: '.min'})).pipe(uglify()).pipe(gulp.dest('build/js')).pipe(connect.reload());
+      });
+
+      gulp.task('watch', function() {
+          gulp.watch('src/sass/*.scss', ['styles']);
+          gulp.watch('src/js/*.js', ['scripts']);
+      });
+
+      gulp.task('default', ['styles', 'scripts', 'watch', 'connect']);
